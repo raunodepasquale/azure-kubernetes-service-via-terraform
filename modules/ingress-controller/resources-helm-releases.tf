8723fb3fd5453ea2c53be6a4b3b30751ffc49cd2
Rauno De Pasquale <rauno@MacPro13Rauno.station>
2020-11-18 11:21:40 +0100

    Ingress controller from kubernetes provider to helm provider

---
+++ modules/ingress-controller/resources-helm-releases.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "ingress-nginx" {
+  name  = "ingress-nginx"
+  chart = "ingress-nginx"
+  repository = "https://kubernetes.github.io"
+  version = "v3.10.1"
+
+  set {
+    namespace  = var.namespace
+  }
+  depends_on = [
+    null_resource.delay,
+    kubernetes_namespace.cert-manager
+  ]
+}
+
